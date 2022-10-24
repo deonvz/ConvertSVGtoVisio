7aa81676cdeb54717d6ea23f455a379dff137628
Deon van Zyl <deon.vanzyl@mweb.co.za>
2022-10-24 14:17:19 +0200

    works with Visio installed

---
+++ ConvertSVGtoVisio/ComObj.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+public class ComObj
+{
+    public static ComObj Create(string progId)
+    {
+        var type = Type.GetTypeFromProgID(progId);
+        if (type == null)
+        {
+            throw new Exception("Servers need to be installed" + progId + "To use this feature");
+        }
+        return new ComObj(Activator.CreateInstance(type));
+    }
+
+    private object _val;
+    public object Val
+    {
+        get { return _val; }
+    }
+    public ComObj(object comObject)
+    {
+        _val = comObject;
+    }
+
+    public ComObj Call(string mehtod, params object[] args)
+    {
+        if (_val == null)
+            return null;
+        var ret = _val.GetType().InvokeMember(mehtod, BindingFlags.InvokeMethod, null, _val, args);
+        return new ComObj(ret);
+    }
+    public ComObj this[string property]
+    {
+        get
+        {
+            if (_val == null)
+                return null;
+            var ret = _val.GetType().InvokeMember(property, BindingFlags.GetProperty, null, _val, null);
+            return new ComObj(ret);
+        }
+        set
+        {
+            if (_val != null)
+                _val.GetType().InvokeMember(property, BindingFlags.SetProperty, null, _val, new object[] { value.Val });
+        }
+    }
+
+    public static void ConvertSvgtoVsdx(string svgFn, string desVsdFn)
+    {
+        var app = ComObj.Create("Visio.Application"); // Required Visio to be installed locally
+        try
+        {
+            // Vidio Required to generate the vsdx file for visio
+            app["Visible"] = new ComObj(false);
+            var docs = app["Documents"];
+            short visOpenHidden = 64, visOpenRO = 2;
+            var doc = docs.Call("OpenEx", svgFn, visOpenHidden + visOpenRO);
+            doc.Call("SaveAs", desVsdFn);
+            doc.Call("Close");
+
+            var win = app["Window"];
+        }
+        finally
+        {
+            app.Call("Quit");
+        }
+
+    }
+}
+
---
+++ ConvertSVGtoVisio/SvgtoVsdx.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace ConvertSVGtoVisio
+{
+    public partial class SvgtoVsdx : Window
+    {
+        public SvgtoVsdx()
+        {
+            InitializeComponent();
+        }
+
+        private string[] path;
+        private int path_length = 0;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (path_length <= 0)
+                MessageBox.Show("Select the files！");
+            else
+            {
+                progressBar1.Maximum = path_length;
+                progressBar1.Value = 0;
+                for (int i = 0; i < path_length; i++)
+                {
+                    int b = path[i].LastIndexOf('.');
+                    string des = path[i].Substring(0, b);
+                    des += ".vsdx";
+                    try
+                    {
+                        ComObj.ConvertSvgtoVsdx(path[i], des);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Is visio installed?" + "Failed:" + path[i], "Failed to Convert this SVG");
+                    }
+                    progressBar1.Value += 1;      
+                    runprocess.Content ="(" + progressBar1.Value.ToString() + "/ " + path_length.ToString() + ")";
+                }
+                path_length = 0;
+                MessageBox.Show("Completed！");
+            }
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Files (*.svg)|*.svg"
+            };
+            openFileDialog.Multiselect = true;
+            string text = string.Empty;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                path = openFileDialog.FileNames;
+                path_length = path.Length;
+                runprocess.Content = "(0/" + path_length.ToString() + ")";
+                for (int i = 0; i < path_length; i++)
+                {
+                    if (i == 0)
+                        text = path[0];
+                    else
+                        text += (", " + path[i]);
+                }
+                textBox1.Text = text;
+            }
+        }
+    }
+}
